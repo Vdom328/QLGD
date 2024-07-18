9931c3d132ffcb25061c861ef7bfed58dcd2471b
vudat <91863066+Vdom328@users.noreply.github.com>
2024-07-18 22:00:24 +0700

    fixx foodter

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="app-footer__inner text-center">
         <div class="app-footer-center">
             <div>©︎ Quản lý giảng đường</div>
-            <div>Phát triển bởi sinh viên: Nguyễn Trung Đức</div>
+            <div>Phát triển bởi sinh viên: Vũ Tiến Đạt</div>
         </div>
     </div>
 </div>
